228fbb89f6637e5d3a8b394f069ab9739db7ae0e
aswinsuriya16 <aswinsuriya16@gmail.com>
2025-09-03 06:24:06 +0000

    Upvote and downvote

--- app/api/upvote/route.ts
+++ app/api/upvote/route.ts
@@ -1,3 +1,36 @@
-export default async function POST() {
-    
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function POST(req : NextRequest) {
+    const session = await getServerSession();
+    const {tweetId} = await req.json();
+    const prismaClient = new PrismaClient();
+    console.log(session);
+    if(!session) {
+        return NextResponse.json({
+            msg : "Unauthorized !"
+        })
+    }
+    try {
+        await prismaClient.upvote.upsert({
+      where: {
+        userId_tweetId: {
+          userId: Number(session.user.id),
+          tweetId: Number(tweetId),
+        },
+      },
+      update: {},
+      create: {
+        userId: Number(session.user.id),
+        tweetId: Number(tweetId),
+      },
+    });
+    }
+    catch(e) {
+        return NextResponse.json({
+            msg : "Failed to upvote !"
+        })
+    }
+
 }
